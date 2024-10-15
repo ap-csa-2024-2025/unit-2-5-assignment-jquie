9bcf85505702d25923debaa152aa2cae4fbbd784
jquie <jacquelynphan03@gmail.com>
2024-10-14 18:42:48 -0700

    done

--- Main.java
+++ Main.java
@@ -4,6 +4,27 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Circle circ1 = new Circle(10.1);
+    System.out.println(circ1.toString());
+    Circle circ2 = new Circle(14);
+    System.out.println(circ2.toString());
+    Circle circ3 = new Circle(20.5);
+    System.out.println(circ3.toString());
+
+    Scanner userInput = new Scanner(System.in);
+    System.out.println("type a number for length and width");
+    double length = userInput.nextDouble();
+    double width = userInput.nextDouble();
+    Rectangle square = new Rectangle(length);
+    System.out.println("square value " + square);
+    Rectangle rec = new Rectangle (length, width);
+    System.out.println("rectable value " + rec);
+
+    double sideLength = userInput.nextDouble();
+    Rectangle eqSquare = new Rectangle (sideLength);
+    RegularPolygon triangle = new RegularPolygon (sideLength);
+    System.out.println(triangle);
+    System.out.println(eqSquare);
   }
+
 }
